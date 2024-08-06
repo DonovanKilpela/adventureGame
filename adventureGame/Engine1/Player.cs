7c92f5cdce852df3c0c1766dfba3975c9abc091a
Donovan Kilpela <161644962+DonovanKilpela@users.noreply.github.com>
2024-08-05 20:48:09 -0500

    Add files via upload

---
+++ adventureGame/Engine1/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine1
+{
+    public class Player
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int gold {  get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+
+    }
+}
